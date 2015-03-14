d69da86bf1f742b082dca3057a9e400e2c9dc64e
Amro El-Fakharany <amro00765@outlook.com>
2015-03-14 11:37:08 +0100

    added firebird

--- src/Tests/EndToEnd/Engines.FluentMigrator.Tests/TestInitialMigration.cs
+++ src/Tests/EndToEnd/Engines.FluentMigrator.Tests/TestInitialMigration.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NHibernate.Cfg;
+using TestTarget;
 using TestTarget.Firebird;
 using Xunit;
 
 namespace Engines.FluentMigrator.Tests
 {
-	public class TestInitialMigration : IClassFixture<Firebird>, IClassFixture<SQLite>
+	public class TestInitialMigration : IClassFixture<Firebird>
 	{
+        private readonly Database _testTarget;
+		private readonly Configuration _nhConfig;
+
+		public TestInitialMigration(Firebird testTarget)
+		{
+			_testTarget = testTarget;
+			_testTarget.CreateDatabase();
+
+			_nhConfig = new Configuration();
+			_nhConfig.DataBaseIntegration(_testTarget.ConfigureDialect);
+		}
 	}
 }
